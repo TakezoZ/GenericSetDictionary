cb0230e93997922da62cd61a2393e024b6fb561b
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 19:33:10 -0300

    V1-Exemplos de Generics(Problema motivador1(reuso)

---
+++ ExempleGenerics/ExempleGenerics/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExempleGenerics
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values: ");
+            int values = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < values; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                printService.AddValue(value);
+            }
+
+            printService.Print();
+            Console.WriteLine($"First: {printService.First()}");
+        }
+    }
+}
