a5ba205b64c20a132320aec8c44d334c78ba90f9
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 21:49:33 -0300

    V2-HashSet e SortedSet(SortedSet{Union,Intersection e difference})

--- HashSet_SortedSet/HashSet_SortedSet/Program.cs
+++ HashSet_SortedSet/HashSet_SortedSet/Program.cs
@@ -4,18 +4,32 @@
     {
         static void Main(string[] Args)
         {
-            HashSet<string> set = new HashSet<string>();
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-            set.Add("TV");
-            set.Add("Notebook");
-            set.Add("Tablet");
+            // union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollections(c);
 
-            Console.WriteLine(set.Contains("Computer"));
+            // intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollections(d);
 
-            foreach (string p in set)
+            // difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollections(e);
+        }
+
+        static void PrintCollections<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
             {
-                Console.WriteLine(p);
+                Console.Write($"{obj} ");
             }
+            Console.WriteLine();
         }
     }
 }
