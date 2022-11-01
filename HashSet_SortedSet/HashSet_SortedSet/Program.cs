3943bd5ad46f2d4dbee680ab9bd1950a93ace61f
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 21:37:42 -0300

    V1-HashSet e SortedSet(HashSet Only)

---
+++ HashSet_SortedSet/HashSet_SortedSet/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HashSet_SortedSet
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
