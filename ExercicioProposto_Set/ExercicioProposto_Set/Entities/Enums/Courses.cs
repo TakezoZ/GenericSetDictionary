15b88f59dc8a2a70b265436623661ec03aad2316
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 23:00:33 -0300

    V1-Exercicio Proposto(set){Minha versão}

---
+++ ExercicioProposto_Set/ExercicioProposto_Set/Entities/Enums/Courses.cs
@@ -0,0 +1,9 @@
+﻿namespace ExercicioProposto_Set.Entities.Enums
+{
+    internal enum Courses
+    {
+        A = 0,
+        B = 1,
+        C = 2,
+    }
+}
---
+++ ExercicioProposto_Set/ExercicioProposto_Set/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExercicioProposto_Set.Entities;
+using ExercicioProposto_Set.Entities.Enums;
+
+namespace ExercicioProposto_Set
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            HashSet<Student> set = new HashSet<Student>();
+
+            Courses course = Courses.A;
+
+            while (course <= Courses.C)
+            {
+                Console.Write($"How many students for course {course}?: ");
+                int a = int.Parse(Console.ReadLine());
+                for (int i = 0; i < a; i++)
+                {
+                    set.Add(new Student(int.Parse(Console.ReadLine())));
+                }
+                course++;
+            }
+            Console.Write($"Total students: {set.Count}");
+        }
+    }
+}
