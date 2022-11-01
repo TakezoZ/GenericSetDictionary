ddd4de14be94d2a28b4ace2c280763229d3897a0
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 22:32:45 -0300

    Exercicio resolvido sobre conjuntos

---
+++ ExercicioResolvido/ExercicioResolvido/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExercicioResolvido.Entities;
+using System.Globalization;
+
+namespace ExercicioResolvido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        set.Add(new LogRecord { Username = line[0], Instant = DateTime.Parse(line[1])});
+                    }
+                    Console.Write($"Total users: {set.Count}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
