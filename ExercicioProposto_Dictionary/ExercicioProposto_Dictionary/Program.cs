8b20d87b004e52da8f0e6d8d08da7fb3683a1567
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-11-01 00:25:27 -0300

    Exercicio proposto(Dictionary)

---
+++ ExercicioProposto_Dictionary/ExercicioProposto_Dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ExercicioProposto_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> canditates = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (canditates.ContainsKey(candidate))
+                        {
+                            canditates[candidate] += votes;
+                        }
+                        else
+                        {
+                            canditates[candidate] = votes;
+                        }
+                        
+                    }
+                    foreach (KeyValuePair<string, int> candidate in canditates)
+                    {
+                        Console.WriteLine($"{candidate.Key}: {candidate.Value}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
