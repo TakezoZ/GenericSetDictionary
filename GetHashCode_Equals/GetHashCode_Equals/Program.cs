399935a11c2d9f0815b986cc2cfbdaf13e328efe
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-31 21:27:53 -0300

    GetHashCode e Equals

---
+++ GetHashCode_Equals/GetHashCode_Equals/Program.cs
@@ -0,0 +1,18 @@
+﻿using GetHashCode_Equals.Entities;
+
+namespace GetHashCode_Equals
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            Client a = new Client{ Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
